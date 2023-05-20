73780c6b3f0470c2795708efc04c925445b18291
Karol Gardyjas <karolgardyjas@gmail.com>
2023-05-20 12:48:22 +0200

    fix: import missing component

--- pages/mountains/_slug.vue
+++ pages/mountains/_slug.vue
@@ -1,10 +1,6 @@
 <template>
   <article>
-    <SocialHead
-      :title="mountain.title"
-      :description="mountain.description"
-      :image="mountain.image"
-    />
+    <SocialHead :title="mountain.title" :description="mountain.description" :image="mountain.image" />
     <h1>{{ mountain.title }}</h1>
     <section>
       <img :src="mountain.image" :alt="mountain.title" />
@@ -14,29 +10,29 @@
   </article>
 </template>
 <script>
+import SocialHead from '@/components/SocialHead'
 export default {
   async asyncData({ params }) {
-    const mountain = await fetch(
-      `https://api.nuxtjs.dev/mountains/${params.slug}`
-    ).then((res) => res.json())
-    return { mountain }
+    const mountain = await fetch(`https://api.nuxtjs.dev/mountains/${params.slug}`).then((res) => res.json());
+    return { mountain };
   },
   methods: {
     goBack() {
-      return this.$router.go(-1)
+      return this.$router.go(-1);
     }
   },
   head() {
     return {
       link: [
         {
-          hid: 'canonical',
-          rel: 'canonical',
+          hid: "canonical",
+          rel: "canonical",
           href: `https://nuxtjs.org/mountains/${this.$route.params.slug}`
         }
       ]
-    }
-  }
+    };
+  },
+  components: { SocialHead }
 }
 </script>
 <style scoped>
@@ -44,9 +40,11 @@ article {
   max-width: 600px;
   margin: 0 auto;
 }
+
 img {
   height: 200px;
 }
+
 p {
   text-align: left;
 }
